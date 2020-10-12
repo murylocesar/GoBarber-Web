9ec39cf36236fed557720cd65749c41b84e9d0c5
Murylo Cesar <murylocesar2014@gmail.com>
2020-10-12 07:57:11 -0300

    Criação da página de redefinição de senha

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,6 +5,7 @@ import { Switch, Route } from 'react-router-dom';
 import SignUp from '../pages/SignUp';
 import Signin from '../pages/Signin';
 import ForgotPassword from '../pages/ForgotPassword';
+import ResetPassword from '../pages/ResetPassword';
 
 // import Dashboard from '../pages/Dashboard';
 
@@ -13,6 +14,7 @@ const Routes: React.FC = () => (
     <Route path="/" exact component={Signin} />
     <Route path="/signup" component={SignUp} />
     <Route path="/forgot-password" component={ForgotPassword} />
+    <Route path="/reset-password" component={ResetPassword} />
 
     {/* <Route path="/Dashboard" component={Dashboard} isPrivate /> */}
   </Switch>
